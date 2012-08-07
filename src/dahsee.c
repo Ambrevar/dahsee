78bde63727ac13ec51d2e5b6899e25dae0147f70
Ambrevar <ambrevar@gmail.com>
2012-08-07 08:52:10 +0100

    Introspection added to queryBus.

--- src/dahsee.c
+++ src/dahsee.c
@@ -139,13 +139,15 @@ timer_print ()
  * current session / system. We use one function to centralize this: the
  * 'query' parameter will define what information we want.
  */
+// TODO: stupid enum?
 enum Queries
 {
     QUERY_LIST_NAMES = 1,
     QUERY_ACTIVATABLE_NAMES = 2,
     QUERY_GET_NAME_OWNER = 4,
     QUERY_GET_CONNECTION_UNIX_USER = 8,
-    QUERY_GET_CONNECTION_UNIX_PROCESS_ID = 16
+    QUERY_GET_CONNECTION_UNIX_PROCESS_ID = 16,
+    QUERY_INTROSPECT = 32
 };
 
 void
@@ -223,6 +225,16 @@ queryBus(int query, char* parameter)
                                                 DBUS_INTERFACE_DBUS,
                                                 "GetConnectionUnixProcessID");
         need_parameter = true;
+        break;
+    }
+
+    case QUERY_INTROSPECT:
+    {
+        // TODO: Need to check if parameter is a valid busname.
+        message = dbus_message_new_method_call (parameter,
+                                                DBUS_PATH_DBUS,
+                                                "org.freedesktop.DBus.Introspectable",
+                                                "Introspect");
         break;
     }
 
@@ -317,6 +329,7 @@ queryBus(int query, char* parameter)
         }
 
         case QUERY_GET_NAME_OWNER:
+        case QUERY_INTROSPECT:
         {
             if (DBUS_TYPE_STRING == dbus_message_iter_get_arg_type (&args))
             {
@@ -1016,7 +1029,9 @@ spy ( /* char* param */ )
 
 /**
  * XML Parser
+ *
  * TODO: support escaped character for delimiters.
+ *
  * Nodes can be
  * 1. Object
  * 2. Interfaces
@@ -1148,95 +1163,6 @@ xmlparser (char *source)
             }
         }
     }
-}
-
-void
-introspect ()
-{
-    // Send method call.
-    DBusMessage *msg;
-    DBusMessageIter args;
-    /* DBusMessageIter name_list; */
-    DBusConnection *conn;
-    DBusError err;
-    DBusPendingCall *pending;
-    char *name;
-
-    // Init error
-    dbus_error_init (&err);
-
-    conn = dbus_bus_get (DBUS_BUS_SESSION, &err);
-    if (dbus_error_is_set (&err))
-    {
-        fprintf (stderr, "Connection Error (%s)", err.message);
-        dbus_error_free (&err);
-    }
-    if (conn == NULL)
-    {
-        exit (1);
-    }
-
-    // create a new method call and check for errors
-    msg = dbus_message_new_method_call (DBUS_SERVICE_DBUS,      // target for the method call object to call on.
-                                        /* "org.naquadah.awesome.awful", */
-                                        // TODO: Useless?????
-                                        DBUS_PATH_DBUS,
-                                        /* DBUS_INTERFACE_DBUS, // interface to call on */
-                                        "org.freedesktop.DBus.Introspectable",  // interface to call on
-                                        "Introspect");  // method name
-
-    if (NULL == msg)
-    {
-        fprintf (stderr, "Message Null\n");
-        exit (1);
-    }
-
-    // send message and get a handle for a reply
-    if (!dbus_connection_send_with_reply (conn, msg, &pending, -1))
-    {                           // -1 is default timeout
-        fprintf (stderr, "Out Of Memory!\n");
-        exit (1);
-    }
-    if (NULL == pending)
-    {
-        fprintf (stderr, "Pending Call Null\n");
-        exit (1);
-    }
-    dbus_connection_flush (conn);
-
-    printf ("Request Sent\n");
-
-    // free message
-    dbus_message_unref (msg);
-
-    // block until we recieve a reply
-    dbus_pending_call_block (pending);
-
-    // get the reply message
-    msg = dbus_pending_call_steal_reply (pending);
-    if (NULL == msg)
-    {
-        fprintf (stderr, "Reply Null\n");
-        exit (1);
-    }
-
-    // free the pending message handle
-    dbus_pending_call_unref (pending);
-
-    // read the parameters
-    if (!dbus_message_iter_init (msg, &args))
-        fprintf (stderr, "Message has no arguments!\n");
-    else if (DBUS_TYPE_STRING != dbus_message_iter_get_arg_type (&args))
-        fprintf (stderr, "Argument is not a string!\n");
-
-    dbus_message_iter_get_basic (&args, &name);
-
-    printf ("Intro: %s\n", name);
-    xmlparser (name);
-
-    // free reply and close connection
-    dbus_message_unref (msg);
-
 }
 
 /**
@@ -1381,7 +1307,7 @@ main (int argc, char **argv)
     extern int optind;
     extern int optopt;
 
-    while ((c = getopt (argc, argv, ":adhln:p:svu:")) != -1)
+    while ((c = getopt (argc, argv, ":adhi:ln:p:svu:")) != -1)
     {
         switch (c)
         {
@@ -1393,6 +1319,9 @@ main (int argc, char **argv)
             break;
         case 'h':
             print_help ();
+            return 0;
+        case 'i':
+            queryBus (QUERY_INTROSPECT, optarg);
             return 0;
         case 'l':
             queryBus (QUERY_LIST_NAMES, NULL);
